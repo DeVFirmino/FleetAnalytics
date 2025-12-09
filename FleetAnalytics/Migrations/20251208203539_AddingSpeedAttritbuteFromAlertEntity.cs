20b19585a831e13186daa306eb7cd42af6f7e42e
devfirmino <daanspfc@gmail.com>
2025-12-09 13:54:14 +0100

    Add assets folder with screenshots for README

---
+++ FleetAnalytics/Migrations/20251208203539_AddingSpeedAttritbuteFromAlertEntity.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FleetAnalytics.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddingSpeedAttritbuteFromAlertEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "Speed",
+                table: "Alerts",
+                type: "REAL",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Speed",
+                table: "Alerts");
+        }
+    }
+}
